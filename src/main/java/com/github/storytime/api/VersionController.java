cfdad0dbf74e9fdb294cd298f1b42e19e53d916e
Bo <bogdan.fedorchenko@infopulse.com>
2018-07-24 21:49:40 +0300

    Merged move-to-circle-ci into develop

--- src/main/java/com/github/storytime/api/VersionController.java
+++ src/main/java/com/github/storytime/api/VersionController.java
@@ -2,7 +2,7 @@ package com.github.storytime.api;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.BufferedReader;
@@ -12,14 +12,13 @@ import java.util.stream.Collectors;
 
 import static com.github.storytime.config.Constants.*;
 import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 public class VersionController {
 
     private static final Logger LOGGER = LogManager.getLogger(VersionController.class);
 
-    @RequestMapping(value = API_PREFIX + "/version", produces = TEXT_PLAIN_VALUE, method = GET)
+    @GetMapping(value = API_PREFIX + "/version", produces = TEXT_PLAIN_VALUE)
     public String getVersion() {
         try (final InputStream is = getClass().getClassLoader().getResourceAsStream(VERSION_PROPERTIES)) {
             final BufferedReader reader = new BufferedReader(new InputStreamReader(is));
