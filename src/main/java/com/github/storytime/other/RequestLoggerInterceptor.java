c1ebc084b0912c75a6da9b052e7765a68aedf8a7
Bogdan.Fedorchenko <Bogdan.Fedorchenko@evry.com>
2018-08-08 22:35:30 +0300

    -- sonar refactoring

--- src/main/java/com/github/storytime/other/RequestLoggerInterceptor.java
+++ src/main/java/com/github/storytime/other/RequestLoggerInterceptor.java
@@ -20,15 +20,15 @@ public class RequestLoggerInterceptor implements ClientHttpRequestInterceptor {
     @Override
     public ClientHttpResponse intercept(final HttpRequest request, final byte[] body,
                                         final ClientHttpRequestExecution execution) throws IOException {
-        traceRequest(request, body);
+        traceRequest(body);
         final ClientHttpResponse response = execution.execute(request, body);
         traceResponse(response);
         return response;
     }
 
-    private void traceRequest(final HttpRequest request, final byte[] body) {
+    private void traceRequest(final byte[] body) {
         try {
-            final String message = "=============================request begin================================================\n" +
+            final String message = "\n=============================request begin================================================\n" +
                     "Request body: " + new String(body, UTF_8) +
                     "\n=============================request end================================================";
             LOGGER.trace(message);
@@ -42,7 +42,7 @@ public class RequestLoggerInterceptor implements ClientHttpRequestInterceptor {
             final String message = "\n=============================response begin================================================\n" +
                     "Response code:     " + response.getRawStatusCode() + "\n" +
                     "Headers:           " + response.getHeaders() + "\n" +
-                    "=============================response end================================================\n";
+                    "=============================response end================================================";
             LOGGER.trace(message);
         } catch (final Exception e) {
             LOGGER.error("Cannot trace response: ", e);
