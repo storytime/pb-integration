c767356cb291278499a43723e9025f635091e0e0
Bogdan.Fedorchenko <Bogdan.Fedorchenko@evry.com>
2018-08-15 00:06:26 +0300

    -- add logger

--- src/main/java/com/github/storytime/service/CustomPayeeService.java
+++ src/main/java/com/github/storytime/service/CustomPayeeService.java
@@ -1,6 +1,7 @@
 package com.github.storytime.service;
 
 import com.github.storytime.model.db.CustomPayee;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,9 +9,12 @@ import java.util.Set;
 
 import static com.github.storytime.config.props.Constants.EMPTY;
 import static java.util.Optional.ofNullable;
+import static org.apache.logging.log4j.LogManager.getLogger;
 
 @Service
 public class CustomPayeeService {
+
+    private static final Logger LOGGER = getLogger(CustomPayeeService.class);
 
     private final Set<CustomPayee> customPayeeValues;
 
@@ -21,11 +25,13 @@ public class CustomPayeeService {
 
     public String getNicePayee(final String maybePayee) {
         final String originalPayee = ofNullable(maybePayee).orElse(EMPTY);
-        return customPayeeValues
+        final String customPayee = customPayeeValues
                 .stream()
                 .filter(cp -> originalPayee.contains(cp.getContainsValue()))
                 .findAny()
                 .map(CustomPayee::getPayee)
                 .orElse(originalPayee).trim();
+        LOGGER.debug("Found custom payee: {}", customPayee);
+        return customPayee;
     }
 }
