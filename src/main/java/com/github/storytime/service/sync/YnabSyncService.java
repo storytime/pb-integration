f9bb5fda127dcdee05b18f6f3ad4cd731aba2ac3
bo <bogdan.fedorchenko@skf.com>
2020-01-26 19:37:54 +0200

    -- change to approved true

--- src/main/java/com/github/storytime/service/sync/YnabSyncService.java
+++ src/main/java/com/github/storytime/service/sync/YnabSyncService.java
@@ -338,7 +338,7 @@ public class YnabSyncService {
         ynabTransactions.setCategoryId(tagId);
         ynabTransactions.setPayeeName(zTr.getPayee());
         ynabTransactions.setCleared(CLEARED);
-        ynabTransactions.setApproved(false);
+        ynabTransactions.setApproved(true);
         ynabTransactions.setImportId(zTr.getId());
 
         return ynabTransactions;
