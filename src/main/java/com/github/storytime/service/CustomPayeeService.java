fecaf66b8d6685d1732be60541b62be2984a72b0
bo <bogdan.fedorchenko@skf.com>
2020-07-08 11:45:14 +0300

    -- add logs for payeer

--- src/main/java/com/github/storytime/service/CustomPayeeService.java
+++ src/main/java/com/github/storytime/service/CustomPayeeService.java
@@ -1,6 +1,8 @@
 package com.github.storytime.service;
 
+import com.github.storytime.mapper.PbToZenTransactionMapper;
 import com.github.storytime.model.db.CustomPayee;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,11 +10,13 @@ import java.util.Set;
 
 import static com.github.storytime.config.props.Constants.EMPTY;
 import static java.util.Optional.ofNullable;
+import static org.apache.logging.log4j.LogManager.getLogger;
 
 @Service
 public class CustomPayeeService {
 
     private final Set<CustomPayee> customPayeeValues;
+    private static final Logger LOGGER = getLogger(CustomPayeeService.class);
 
     @Autowired
     public CustomPayeeService(final Set<CustomPayee> customPayeeValues) {
@@ -20,12 +24,16 @@ public class CustomPayeeService {
     }
 
     public String getNicePayee(final String maybePayee) {
-        final String originalPayee = ofNullable(maybePayee).orElse(EMPTY);
-        return customPayeeValues
+        var originalPayee = ofNullable(maybePayee).orElse(EMPTY);
+        var nicePayee = customPayeeValues
                 .stream()
                 .filter(cp -> originalPayee.contains(cp.getContainsValue()))
                 .findAny()
                 .map(CustomPayee::getPayee)
                 .orElse(originalPayee).trim();
+
+        LOGGER.debug("Nice payee is: [{}] for original: [{}]", nicePayee, originalPayee);
+
+        return nicePayee;
     }
 }
