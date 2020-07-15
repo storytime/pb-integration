a0065d017abbb0047a85cf09e203cacd590e6c3b
bo <bogdan.fedorchenko@skf.com>
2020-07-15 10:49:06 +0300

    -- change merchant logic

--- src/main/java/com/github/storytime/service/http/ZenDiffHttpService.java
+++ src/main/java/com/github/storytime/service/http/ZenDiffHttpService.java
@@ -85,7 +85,7 @@ public class ZenDiffHttpService {
     public String findMerchantByNicePayee(final ZenResponse zenDiff, final String nicePayee) {
         return ofNullable(zenDiff.getMerchant()).orElse(emptyList())
                 .stream()
-                .filter(a -> ofNullable(a.getTitle()).orElse(EMPTY).equalsIgnoreCase(nicePayee))
+                .filter(a -> ofNullable(a.getTitle()).orElse(EMPTY).trim().equals(nicePayee))
                 .findFirst()
                 .map(MerchantItem::getId)
                 .orElse(null);
