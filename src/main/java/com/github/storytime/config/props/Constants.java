3f98053bd01a0e551c07090e14a68110414c3d0b
bo <bogdan.fedorchenko@skf.com>
2020-07-27 10:41:21 +0300

    -- refactoring

--- src/main/java/com/github/storytime/config/props/Constants.java
+++ src/main/java/com/github/storytime/config/props/Constants.java
@@ -70,6 +70,9 @@ public class Constants {
     public static final String TAG = "tag";
     public static final String SLASH = "/";
     public static final BigDecimal DEFAULT_ACC_BALANCE = BigDecimal.valueOf(0);
+    public static final String SPLITTER = ", ";
+    public static final String PR = "[";
+    public static final String SUF = "]";
 
     private Constants() {
     }
